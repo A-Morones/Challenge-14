e57af245a46a6fd5f59c220039caabf0b585e45f
Adalberto Morones <adalbertomorones@yahoo.com>
2024-08-20 18:43:32 -0500

    made changes to files and added new file to seeds folder.

---
+++ seeds/userData.js
@@ -0,0 +1,34 @@
+const { User } = require("../models");
+
+const userData = [
+  {
+    username: "user1",
+    email: "user1@example.com",
+    password: "password1",
+  },
+  {
+    username: "user2",
+    email: "user2@example.com",
+    password: "password2",
+  },
+  {
+    username: "user3",
+    email: "user3@example.com",
+    password: "password3",
+  },
+  {
+    username: "user4",
+    email: "user4@example.com",
+    password: "password4",
+  },
+  {
+    username: "user5",
+    email: "user5@example.com",
+    password: "password5",
+  },
+
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
